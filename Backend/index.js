039c25503a7d84a65e4c18f572c956ae6fef07c8
Harsh-Yadav029 <156350591+Harsh-Yadav029@users.noreply.github.com>
2025-12-23 23:32:48 +0530

    server error

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser())
 app.use(express.json())
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://blog-site-uypo.vercel.app"
+  "https://blog-site-ps2h.vercel.app/"
 ];
 
 app.use(cors({
